afb0708639792ac8390fdbb3c2bcfe78c25cad11
hossainchisty <hossain.chisty11@gmail.com>
2021-04-25 12:01:14 +0600

    remove overview url

--- api/urls.py
+++ api/urls.py
@@ -3,9 +3,11 @@ from . import views
 
 urlpatterns = [
     
-    path('', views.OverviewApi, name='apiview'),
+    # http://127.0.0.1:8000/api/task-list
+
     path('task-list/', views.taskList, name='task-list'),
     path('task-create/', views.taskCreate, name='task-create'),
+    
     path('task-update/<str:pk>/', views.taskUpdate, name='task-update'),
     path('task-delete/<str:pk>/', views.taskDelete, name='task-delete'),
     path('task-details/<str:pk>/', views.taskDetail, name='task-detail'),
