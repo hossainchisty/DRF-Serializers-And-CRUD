59dbc9fdd9229f28be665801600c1d96142bd1c9
hossainchisty <hossain.chisty11@gmail.com>
2021-04-25 11:48:56 +0600

    add files

---
+++ api/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    completed = models.BooleanField(default=False,null=True,blank=True)
+
+    class Meta:
+        ordering = ['title']
+        
+    def __str__(self):
+        return self.title
+
+        
---
+++ api/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    
+    path('', views.OverviewApi, name='apiview'),
+    path('task-list/', views.taskList, name='task-list'),
+    path('task-create/', views.taskCreate, name='task-create'),
+    path('task-update/<str:pk>/', views.taskUpdate, name='task-update'),
+    path('task-delete/<str:pk>/', views.taskDelete, name='task-delete'),
+    path('task-details/<str:pk>/', views.taskDetail, name='task-detail'),
+
+]
