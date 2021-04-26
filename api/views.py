ca3f7dcf046854519c7714a2248b88549d777945
hossainchisty <hossain.chisty11@gmail.com>
2021-04-26 17:11:37 +0600

    Update views.py

--- api/views.py
+++ api/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .models import Task
 from .serializers import TaskSerializer
+from .models import Task
 # Create your views here.
 
 @api_view(['GET'])
